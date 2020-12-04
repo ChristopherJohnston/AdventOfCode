f242c6c8d8cc8391d2a7a310e92cf020d1d75450
Chris Johnston <christopher_johnston@me.com>
2020-12-04 07:48:57 +0000

    Day 4 improvements

--- 2020/Day4/app/Program.cs
+++ 2020/Day4/app/Program.cs
@@ -10,65 +10,34 @@ namespace app
     {
         static void Main(string[] args)
         {
-            int nValid = 0;
+            bool skipValidation = false; // true for part 1, false for part2
 
-            foreach (string passport in ParseInput()) {
-                int nKeys = 0;
+            Func<string,int,int, bool> IsValidYear = (value, min, max) => {
+                int year;
+                return (int.TryParse(value, out year) && year>=min && year<=max);
+            };
 
-                // Parse the key-value pairs into a dictionary. pairs separated by spaces, key-value separated by colons
-                Dictionary<string, string> passportDict = new Dictionary<string, string>();
-
-                foreach (string kv in passport.Split(' ')) {
-                    string[] pair = kv.Split(':');
-                    passportDict.Add(pair[0], pair[1]);
-                }
-
-                // Check for too few or too many fields
-                if (passportDict.Keys.Count < 7 || passportDict.Keys.Count > 8) {
-                    continue;
-                }
+            int nValid = ParseInput().Select( (passport) => {
+                var dict = passport.Split(' ').Select(p=>p.Split(':')).ToDictionary(sp => sp[0], sp=>sp[1]);
                 
                 // Required keys and their validtors
-                (string, Func<string, bool>)[] requiredKeys = new (string, Func<string, bool>)[] {
+                return new (string key, Func<string, bool> validator)[] {
                     ("byr", (s)=> IsValidYear(s, 1920, 2002)),
                     ("iyr", (s) => IsValidYear(s, 2010, 2020)),
                     ("eyr", (s) => IsValidYear(s, 2020, 2030)),
-                    ("hgt", IsValidHeight),
+                    ("hgt", (s) => {
+                        int value;
+                        int.TryParse(s.Substring(0, s.Length-2), out value);
+                        string unit = s.Substring(s.Length-2, 2);
+                        return ((unit == "cm" && value >= 150 && value <= 193) || (unit == "in" && value >=59 && value<=76));
+                    }),
                     ("hcl", (s) => Regex.IsMatch(s, @"^\#[0-9a-f]{6}$")),
                     ("ecl", (s) => new[] { "amb", "blu", "brn", "gry", "grn", "hzl", "oth"}.Contains(s)),
                     ("pid", (s) => Regex.IsMatch(s, @"^[0-9]{9}$"))
-                };
-
-                foreach(var kv in requiredKeys) {
-                    // all required keys must be present and valid
-                    if (passportDict.ContainsKey(kv.Item1) && kv.Item2(passportDict[kv.Item1])) {
-                        nKeys++;
-                    } else {
-                        break;
-                    }
-                }
-
-                if (nKeys == requiredKeys.Length) {
-                    nValid++;
-                }
-            }
+                }.Select((kv) => (dict.ContainsKey(kv.key) && (skipValidation || kv.validator(dict[kv.key])))).All((i)=>i);
+            }).Count((b) => b);
 
             Console.WriteLine(nValid);
-        }
-
-        static bool IsValidYear(string value, int min, int max) {
-            int year;
-            return (int.TryParse(value, out year) && year>=min && year<=max);
-        }
-
-        static bool IsValidHeight(string height) {
-            int value;
-            if (!int.TryParse(height.Substring(0, height.Length-2), out value)) {
-                return false;
-            }
-
-            string unit = height.Substring(height.Length-2, 2);
-            return ((unit == "cm" && value >= 150 && value <= 193) || (unit == "in" && value >=59 && value<=76));
         }
 
         static IEnumerable<string> ParseInput() {
