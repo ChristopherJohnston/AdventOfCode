fe83d9c4e9c9bdaf5e1ebce09770b66e8529093c
Chris Johnston <christopher_johnston@me.com>
2020-12-06 09:30:07 +0000

    Solved Part 1 of 2020 Day 6

---
+++ 2020/Day6/app/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Linq;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ParseInput().Select((p) => p.ToCharArray().Distinct().Count()).Sum());
+        }
+
+        static IEnumerable<string> ParseInput() {
+            string[] lines = File.ReadAllLines(@"input.txt");
+            string currentPassport = String.Empty;
+            for (int i=0; i<lines.Length; i++) {
+                if (lines[i] == string.Empty) {
+                    yield return currentPassport.TrimEnd();
+                    currentPassport = String.Empty;
+                } else {
+                    currentPassport += lines[i];
+                }
+            }            
+        }
+    }
+}
