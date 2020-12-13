82096c399fd7e40e3ce7af6684c74a9019154b75
Chris Johnston <christopher_johnston@me.com>
2020-12-13 22:38:57 +0000

    Solved Part 2 of 2020 Day 13

--- 2020/Day13/app/Program.cs
+++ 2020/Day13/app/Program.cs
@@ -14,25 +14,32 @@ namespace app
 
             // long earliest = 939;
             // string[] buses = "7,13,x,x,59,x,31,19".Split(',');
-            Part1(earliest, buses);
+            // string[] buses = "67,7,59,61".Split(',');
+            // Part1(earliest, buses);
             Part2(buses);
         }
 
-        static void Part2(string[] buses) {
-            long i = 100000000000000;
-            bool found = false;
-            while (!found) {
-                i++;
+        public static void Part2(string[] buses)
+        {
+            long timestamp = 1;
+            long multiplier = 1;
 
-                found = buses.Select((busId, idx) => {
-                    if (busId == "x")
-                        return true;
-
-                    long bId = long.Parse(busId);
-                    return (i % bId) == (bId - idx);
-                }).All((b) => b);
+            for (int offset=0; offset<buses.Length; offset++)
+            {
+                if (buses[offset] == "x")
+                    continue;
+                    
+                long id = long.Parse(buses[offset]);
+                bool found = false;
+                while (!found) {
+                    timestamp += multiplier;
+                    found = (timestamp + offset) % id == 0;
+                }
+                multiplier *= id;
+                Console.WriteLine("Iteration {0}: Timestamp={1}, Multiplier={2}", offset, timestamp, multiplier);
             }
-            Console.WriteLine(i);
+
+            Console.WriteLine(timestamp);
         }
 
         static void Part1(long earliest, string[] buses) {
