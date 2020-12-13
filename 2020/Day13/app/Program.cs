c37f5e1d803d9775daefebac7e2cdee1aa8981be
Chris Johnston <christopher_johnston@me.com>
2020-12-13 21:49:56 +0000

    Part 2 but will take forever

--- 2020/Day13/app/Program.cs
+++ 2020/Day13/app/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 namespace app
 {
@@ -9,9 +10,32 @@ namespace app
         {
             string[] input = File.ReadAllLines(@"input.txt");
             long earliest = long.Parse(input[0]);
-            // string[] buses = "7,13,x,x,59,x,31,19".Split(',');
             string[] buses = input[1].Split(',');
 
+            // long earliest = 939;
+            // string[] buses = "7,13,x,x,59,x,31,19".Split(',');
+            Part1(earliest, buses);
+            Part2(buses);
+        }
+
+        static void Part2(string[] buses) {
+            long i = 100000000000000;
+            bool found = false;
+            while (!found) {
+                i++;
+
+                found = buses.Select((busId, idx) => {
+                    if (busId == "x")
+                        return true;
+
+                    long bId = long.Parse(busId);
+                    return (i % bId) == (bId - idx);
+                }).All((b) => b);
+            }
+            Console.WriteLine(i);
+        }
+
+        static void Part1(long earliest, string[] buses) {
             long waitTime = 939;
             long earliestBus = 0;
 
