ba407b6c6047024e96d03d50be07fad51be5cc64
Chris Johnston <christopher_johnston@me.com>
2020-12-14 10:02:01 +0000

    Solved Part 2 of 2020 Day 14

--- 2020/Day14/app/Program.cs
+++ 2020/Day14/app/Program.cs
@@ -10,6 +10,59 @@ namespace app
     {
         static void Main(string[] args)
         {
+            Part1();
+            Part2();
+        }
+
+        static void Part2() {
+            string mask = "";
+            Dictionary<long, long> memory = new Dictionary<long, long>();
+
+            foreach (string command in ParseInput()) {
+                if (command.Contains("mask")) {
+                    mask = Regex.Match(command, @"^mask = (.*)$").Groups[1].Value;
+                } else if (command.Contains("mem")) {
+                    Match m = Regex.Match(command, @"^mem\[(\d+)\] = (\d+)$");
+                    long key = long.Parse(m.Groups[1].Value);
+                    long value = long.Parse(m.Groups[2].Value);
+
+                    string bits = Convert.ToString(key, 2);
+                    List<string> newBits = new List<string>() { String.Empty };
+
+                    for (int i=0; i<mask.Length; i++) {
+                        switch (mask[i]) {
+                            case '1':
+                                for (int j=0; j<newBits.Count; j++) {
+                                    newBits[j] += '1';
+                                }                                
+                                break;
+                            case '0':
+                                for (int j=0; j<newBits.Count; j++) {
+                                    newBits[j] += (i+bits.Length-36 >=0) ? bits[i+bits.Length-36] : '0';
+                                }
+                                break;
+                            case 'X':
+                            default:
+                                List<string> nb = new List<string>();
+                                for (int j=0; j<newBits.Count; j++) {
+                                    nb.Add(newBits[j] + "1");
+                                    newBits[j] += '0';
+                                }
+                                newBits.AddRange(nb);
+                                break;
+                        }
+                    }
+                    
+                    foreach (string n in newBits) {
+                        memory[Convert.ToInt64(n, 2)] = value;
+                    }
+                }
+            }
+
+            Console.WriteLine(memory.Sum((kv) => kv.Value));
+        }
+
+        static void Part1() {
             string mask = "";
             Dictionary<long, long> memory = new Dictionary<long, long>();
 
@@ -30,7 +83,7 @@ namespace app
                             case '0':
                                 newBits += '0';
                                 break;
-                            case 'x':
+                            case 'X':
                             default:
                                 if (i+bits.Length-36 >=0) {
                                     newBits += bits[i+bits.Length-36];
