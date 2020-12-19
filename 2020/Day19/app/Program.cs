c0e524f516d34e29e93e1f7cb332f236b7a38f94
Chris Johnston <christopher_johnston@me.com>
2020-12-19 14:56:22 +0000

    Solved Part 2 of 2020 Day 19

--- 2020/Day19/app/Program.cs
+++ 2020/Day19/app/Program.cs
@@ -22,21 +22,39 @@ namespace app
 
             string[] messages = input.Skip(i).ToArray();
 
-            string ruleZero = rules[0];
-            Regex regex = new Regex(@"[0-9]+");
-            Match m = regex.Match(ruleZero);
-            while (m.Length > 0) {
-                string r = rules[int.Parse(m.Value)];
-                r = (r.Length == 1) ? r : string.Format("({0})", r);
-                ruleZero = regex.Replace(ruleZero, r, 1);
-                m = regex.Match(ruleZero);
-            }
-
-            ruleZero = String.Format("^{0}$",ruleZero.Replace(" ", ""));
+            string ruleZero = String.Format("^{0}$", Solve(rules, 0, int.MaxValue));
             Console.WriteLine(ruleZero.Length);
-
             Console.WriteLine(messages.Count((m) => Regex.IsMatch(m, ruleZero)));
 
+            rules[8] = "42 | 42 8";
+            rules[8] = Solve(rules, 8, 10000);
+            rules[11] = "42 31 | 42 11 31";
+            rules[11] = Solve(rules, 11, 10000);
+
+            ruleZero = String.Format("^{0}$",Solve(rules, 0, int.MaxValue));
+            Console.WriteLine(ruleZero.Length);
+            Console.WriteLine(messages.Count((m) => Regex.IsMatch(m, ruleZero)));
+        }
+        static string Solve(Dictionary<int, string> rules, int number, int maxIterations) {
+            string ruleToSolve = rules[number];
+            Regex regex = new Regex(@"[0-9]+");
+            Match m = regex.Match(ruleToSolve);
+            int iterations=0;
+            while (m.Length > 0) {
+                int n = int.Parse(m.Value);
+                string r = rules[int.Parse(m.Value)];
+                r = (r.Length == 1) ? r : string.Format("({0})", r);
+                ruleToSolve = regex.Replace(ruleToSolve, r, 1);
+                m = regex.Match(ruleToSolve);
+                iterations++;
+                if (n == number && iterations > maxIterations){
+                    ruleToSolve = ruleToSolve.Replace("("+rules[number]+")", "");
+                    break;
+                }
+            }
+
+            ruleToSolve = ruleToSolve.Replace(" ", "");
+            return ruleToSolve;
         }
     }
 }
