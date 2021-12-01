07e762389853f7c3a14ec6c61b5d2ef74f1712a4
Chris Johnston <christopher_johnston@me.com>
2021-12-01 10:24:43 +0000

    Solved Part2 of 2021 Day1

--- 2021/Day1/app/Program.cs
+++ 2021/Day1/app/Program.cs
@@ -8,9 +8,15 @@ namespace app
 {
     class Program
     {
+        static string file = @"input.txt";
         static void Main(string[] args)
         {
+            if (args.Length > 0) {
+                file = args[0];
+            }
+
             Part1();
+            Part2();
         }
 
         static void Part1() {
@@ -25,9 +31,39 @@ namespace app
             }
             Console.WriteLine(numIncreases);
         }
+
+        static void Part2() {
+            long[] prevMeasurements = new long[3];
+            long[] currMeasurements = new long[3];
+            int i=0;
+
+            int numIncreases = 0;
+
+            foreach (string d in ParseInput()) {
+                long depth = long.Parse(d);
+
+                if (i < 3) {
+                    prevMeasurements[i] = depth;
+                    i++;
+                }
+                else {
+                    currMeasurements[0] = prevMeasurements[1];
+                    currMeasurements[1] = prevMeasurements[2];
+                    currMeasurements[2] = depth;
+
+                    if (prevMeasurements.Sum() < currMeasurements.Sum()) {
+                        numIncreases++;
+                    }
+                    
+                    currMeasurements.CopyTo(prevMeasurements, 0);
+                }
+            }
+
+            Console.WriteLine(numIncreases);
+        }
         
         static IEnumerable<string> ParseInput() {
-            foreach (string line in File.ReadAllLines(@"input.txt")) {
+            foreach (string line in File.ReadAllLines(file)) {
                 yield return line;
             }
         }
