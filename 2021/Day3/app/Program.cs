ee582a79a4e5fcb682183710a1eb899a91a50971
Chris Johnston <christopher_johnston@me.com>
2021-12-03 10:57:21 +0000

    Solved Part 1 of 2021 Day 3

---
+++ 2021/Day3/app/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace app
+{
+    /*
+    --- Day 3: Binary Diagnostic ---
+
+    The submarine has been making some odd creaking noises, so you ask it to produce a diagnostic report just in case.
+
+    The diagnostic report (your puzzle input) consists of a list of binary numbers which, when decoded properly, can tell you many useful things about the conditions of the submarine. The first parameter to check is the power consumption.
+
+    You need to use the binary numbers in the diagnostic report to generate two new binary numbers (called the gamma rate and the epsilon rate). The power consumption can then be found by multiplying the gamma rate by the epsilon rate.
+
+    Each bit in the gamma rate can be determined by finding the most common bit in the corresponding position of all numbers in the diagnostic report. For example, given the following diagnostic report:
+
+    00100
+    11110
+    10110
+    10111
+    10101
+    01111
+    00111
+    11100
+    10000
+    11001
+    00010
+    01010
+    Considering only the first bit of each number, there are five 0 bits and seven 1 bits. Since the most common bit is 1, the first bit of the gamma rate is 1.
+
+    The most common second bit of the numbers in the diagnostic report is 0, so the second bit of the gamma rate is 0.
+
+    The most common value of the third, fourth, and fifth bits are 1, 1, and 0, respectively, and so the final three bits of the gamma rate are 110.
+
+    So, the gamma rate is the binary number 10110, or 22 in decimal.
+
+    The epsilon rate is calculated in a similar way; rather than use the most common bit, the least common bit from each position is used. So, the epsilon rate is 01001, or 9 in decimal. Multiplying the gamma rate (22) by the epsilon rate (9) produces the power consumption, 198.
+
+    Use the binary numbers in your diagnostic report to calculate the gamma rate and epsilon rate, then multiply them together. What is the power consumption of the submarine? (Be sure to represent your answer in decimal, not binary.)
+    */
+    class Program
+    {
+        static string file = @"input.txt";
+
+        static void Main(string[] args)
+        {
+            if (args.Length > 0) {
+                file = args[0];
+            }
+
+            Part1();
+            // Part2();
+        }
+
+        static void Part1() {
+            List<int> c = new List<int>();
+            int rowCount = 0;
+
+            // Go through the input and sum the columns
+            foreach (string d in ParseInput()) {
+                for (int i=0; i<d.Length; i++) {
+                    int val = int.Parse(d[i].ToString());
+
+                    if (rowCount == 0) {
+                        c.Add(0);
+                    }
+
+                    c[i] += val;
+                }
+                rowCount += 1;
+            }
+
+            // Convert the columns into binary string
+            string gamma = "";
+            string epsilon = "";
+
+            foreach (int count in c) {
+                // if the sum of the column is greater than half then there's more ones than zeros.
+                float numOnes = (float)count/rowCount;
+
+                gamma += (numOnes >= 0.5) ? '1' : '0';                
+                epsilon += (numOnes >= 0.5) ? '0' : '1';
+            }
+
+            int gammaInt = Convert.ToInt32(gamma, 2);
+            int epsilonInt = Convert.ToInt32(epsilon, 2);
+
+            Console.WriteLine($"Gamma: {gammaInt}, Epsilon: {epsilonInt}, Multiple: {gammaInt*epsilonInt}");
+        }
+
+        static void Part2() {
+            long horizontal = 0;
+            long depth = 0;
+            long aim = 0;
+
+            foreach (string d in ParseInput()) {
+            }
+
+            Console.WriteLine($"Horizontal: {horizontal}, Depth: {depth}, Aim: {aim}, Answer: {horizontal*depth}");
+        }
+        
+        static IEnumerable<string> ParseInput() {
+            foreach (string line in File.ReadAllLines(file)) {
+                yield return line;
+            }
+        }
+    }
+}
