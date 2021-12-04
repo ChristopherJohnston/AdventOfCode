739764c5a352e28c6e8ecb9e63042249495799b4
Chris Johnston <christopher_johnston@me.com>
2021-12-04 12:38:11 +0000

    Solved Part 1 of 2021 Day 4 - extensions added

--- Common/Common/Extensions.cs
+++ Common/Common/Extensions.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Collections.Generic;
 
 namespace Common {
     public static class StringExtensions {
+        ///
+        /// <summary>Converts the string from binary to an integer (e.g. 1101 -> 13)</summary>
+        ///
         public static int BinaryToInt32(this string str) {
             return Convert.ToInt32(str, 2);
+        }
+
+        ///
+        /// <summary>Writes the string to console.</summary>
+        ///
+        public static void ToConsole(this string str) {
+            Console.WriteLine(str);
+        }
+
+        public static IEnumerable<T> SplitToList<T>(this string str, char separator=',') {
+            List<T> output = new List<T>();
+            foreach (string val in str.Split(separator)) {
+                output.Add((T)Convert.ChangeType(val, typeof(T)));
+            }
+            return output;
         }
     }
 }
