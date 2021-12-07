e0f725a37d611717e3de6f3a8699518e9bcf45ed
Chris Johnston <christopher_johnston@me.com>
2021-12-07 20:46:35 +0000

    Solved Part 1 of 2021 Day 7

---
+++ 2021/Day7/app/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Common;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace app
+{
+    /*
+    --- Day 7: The Treachery of Whales ---
+
+    A giant whale has decided your submarine is its next meal, and it's much faster than you are. There's nowhere to run!
+
+    Suddenly, a swarm of crabs (each in its own tiny submarine - it's too deep for them otherwise) zooms in to rescue you! They seem to be preparing to blast a hole in the ocean floor; sensors indicate a massive underground cave system just beyond where they're aiming!
+
+    The crab submarines all need to be aligned before they'll have enough power to blast a large enough hole for your submarine to get through. However, it doesn't look like they'll be aligned before the whale catches you! Maybe you can help?
+
+    There's one major catch - crab submarines can only move horizontally.
+
+    You quickly make a list of the horizontal position of each crab (your puzzle input). Crab submarines have limited fuel, so you need to find a way to make all of their horizontal positions match while requiring them to spend as little fuel as possible.
+
+    For example, consider the following horizontal positions:
+
+    16,1,2,0,4,2,7,1,2,14
+    This means there's a crab with horizontal position 16, a crab with horizontal position 1, and so on.
+
+    Each change of 1 step in horizontal position of a single crab costs 1 fuel. You could choose any horizontal position to align them all on, but the one that costs the least fuel is horizontal position 2:
+
+    Move from 16 to 2: 14 fuel
+    Move from 1 to 2: 1 fuel
+    Move from 2 to 2: 0 fuel
+    Move from 0 to 2: 2 fuel
+    Move from 4 to 2: 2 fuel
+    Move from 2 to 2: 0 fuel
+    Move from 7 to 2: 5 fuel
+    Move from 1 to 2: 1 fuel
+    Move from 2 to 2: 0 fuel
+    Move from 14 to 2: 12 fuel
+    This costs a total of 37 fuel. This is the cheapest possible outcome; more expensive outcomes include aligning at position 1 (41 fuel), position 3 (39 fuel), or position 10 (71 fuel).
+
+    Determine the horizontal position that the crabs can align to using the least fuel possible. How much fuel must they spend to align to that position?
+    */
+    class Program
+    {
+        static string file = @"input.txt";
+
+        static void Main(string[] args)
+        {
+            if (args.Length > 0) {
+                file = args[0];
+            }
+            
+            List<int> crabPositions = FileUtils.ParseInput(file).ToList().First().SplitToList<int>().ToList();
+            crabPositions.Sort();
+
+            // Part 1 Answer: 336040
+            Console.WriteLine($"Part 1 Answer: {GetPosition(crabPositions)}");
+        }
+
+        static void PrintState(int days, List<int> state) {
+            Console.WriteLine($"After {days} days. {string.Join(',', state)}");
+        }
+
+        static long GetPosition(List<int> crabPositions) {
+            long lowestFuel = long.MaxValue;
+            for (int i=0; i<crabPositions.Count;i++) {
+
+                int currentPos = crabPositions[i];
+                long totalFuel = 0;
+                
+                for (int p=0; p<crabPositions.Count;p++) {
+                    totalFuel += Math.Abs(crabPositions[p] - currentPos);
+                }
+
+                lowestFuel = Math.Min(lowestFuel, totalFuel);
+            }
+
+            return lowestFuel;
+        }
+    }
+}
